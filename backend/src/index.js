c7cc36624dbd9c1f2a518f5ce4289c1c99aaf5bd
vaskez <vsqz2k9@gmail.com>
2026-03-26 19:08:25 +0100

    Fix v1

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on ${HOST}:${PORT}`);
 });
